19b281a5a36ee5be10637812730099328c2b39f0
Emilixipixi <carlosemilio270798@gmail.com>
2026-03-26 18:08:02 -0500

    feat: declarar atributos private y constructor de Carrito

---
+++ Tienda_virtual_Integrante_3/Carrito.java
@@ -0,0 +1,14 @@
+public class Carrito {
+ 
+    private String nombreCliente;
+    private String[] productos;
+    private double[] precios;
+    private int cantidad;
+ 
+    public Carrito(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+        this.productos = new String[10];
+        this.precios = new double[10];
+        this.cantidad = 0;
+    }
+}
