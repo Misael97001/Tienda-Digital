82481ce8cdd7c804fc069b528c9203f865f44d72
Misael97001 <chiluisajefferson97@gmail.com>
2026-03-26 17:56:44 -0500

    Add: Metodos de producto

--- Tienda/Producto.java
+++ Tienda/Producto.java
@@ -1,4 +1,5 @@
 package Tienda;
+
 public class Producto {
     
     // Atributos
@@ -25,7 +26,30 @@ public class Producto {
     public String getCategoria() { return categoria; }
     public void setCategoria(String categoria) { this.categoria = categoria; }
 
+    // Método: mostrarInfo()
+    public void mostrarInfo() {
+        System.out.println("Producto: " + nombre);
+        System.out.println("Categoría: " + categoria);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Stock: " + stock + " unidades");
+    }
 
-    
+    // Método: aplicarDescuento(double porcentaje)
+    public void aplicarDescuento(double porcentaje) {
+        double montoDescontado = precio * (porcentaje / 100);
+        double precioFinal = precio - montoDescontado;
+        
+        System.out.println("Descuento del " + porcentaje + "%: -$" + String.format("%.2f", montoDescontado));
+        System.out.println("Precio con descuento: $" + String.format("%.2f", precioFinal));
+    }
+
+    // Método: verificarStock(int cantidad)
+    public void verificarStock(int cantidad) {
+        if (cantidad <= stock) {
+            System.out.println("Disponible");
+        } else {
+            System.out.println("Stock insuficiente. Disponible: " + stock);
+        }
+    }
     
 }
