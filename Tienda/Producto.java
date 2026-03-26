68604c3c776f21648f7c2a7b8ddc6c9c127099e6
Misael97001 <chiluisajefferson97@gmail.com>
2026-03-26 17:55:29 -0500

    Add: Getters and setters

--- Tienda/Producto.java
+++ Tienda/Producto.java
@@ -15,7 +15,16 @@ public class Producto {
         this.categoria = categoria;
     }    
 
-        
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public double getPrecio() { return precio; }
+    public void setPrecio(double precio) { this.precio = precio; }
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+
 
     
     
